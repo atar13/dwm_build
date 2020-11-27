149e7cbc84bc037588529640d8090eeaed6fdf87
atar13 <atar137h@gmail.com>
2020-11-27 00:30:41 -0800

    indicators, fakefullscreen, barheight, and pertag patches

--- dwm_switch.c
+++ dwm_switch.c
@@ -36,12 +36,12 @@
 #include <X11/Xlib.h>
 #include <X11/Xproto.h>
 #include <X11/Xutil.h>
-#include <X11/Xft/Xft.h>
+// #include <X11/Xft/Xft.h>
 
 #ifdef XINERAMA
 #include <X11/extensions/Xinerama.h>
 #endif /* XINERAMA */
-#include <X11/Xft/Xft.h>
+// #include <X11/Xft/Xft.h>
 
 #include "drw.h"
 #include "util.h"
@@ -2210,23 +2210,5 @@ zoom(const Arg *arg)
 int
 main(int argc, char *argv[])
 {
-	if (argc == 2 && !strcmp("-v", argv[1]))
-		die("dwm-"VERSION);
-	else if (argc != 1)
-		die("usage: dwm [-v]");
-	if (!setlocale(LC_CTYPE, "") || !XSupportsLocale())
-		fputs("warning: no locale support\n", stderr);
-	if (!(dpy = XOpenDisplay(NULL)))
-		die("dwm: cannot open display");
-	checkotherwm();
-	setup();
-#ifdef __OpenBSD__
-	if (pledge("stdio rpath proc exec", NULL) == -1)
-		die("pledge");
-#endif /* __OpenBSD__ */
-	scan();
-	run();
-	cleanup();
-	XCloseDisplay(dpy);
-	return EXIT_SUCCESS;
+	view_adjacent(.i = +1);
 }
